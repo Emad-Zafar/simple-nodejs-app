b82566fd74a00df9ffc450c07fe6afd81d8cef8e
Emad-Zafar <emad.zafar.bsse-2020a@cecosian.edu.pk>
2024-09-23 22:04:48 +0500

    Add CI/CD pipeline with GitHub Actions and Docker

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,11 @@
-# Use an official Node.js runtime as a parent image
-FROM node:14
+FROM node:16
 
-# Set the working directory inside the container
-WORKDIR /usr/src/app
+WORKDIR /myapp
 
-# Copy the package.json and package-lock.json files
 COPY package*.json ./
 
-# Install dependencies
 RUN npm install
 
-# Copy the rest of the app’s source code
 COPY . .
 
-# Expose the port the app runs on
-EXPOSE 3000
-
-# Run tests first, then start the app
-CMD ["npm", "test"] && ["npm", "start"]
+CMD ["npm", "test"]
--- index.js
+++ index.js
@@ -1,69 +1,12 @@
-import express from "express";
-import axios from "axios"; // Use axios instead of request
+const http = require("http");
+const port = process.env.PORT || 3000;
 
-const app = express();
-
-// ejs setup
-app.set("view engine", "ejs");
-
-// Routes
-app.get("/", (req, res) => {
-  res.render("index");
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  const msg = "Hello Node!\n";
+  res.end(msg);
 });
 
-app.get("/index", async (req, response) => {
-  const person = req.query.person;
-
-  if (!person) {
-    return response.redirect("404");
-  }
-
-  try {
-    // Wikipedia API for searching
-    const url = "https://en.wikipedia.org/w/api.php";
-    const params = {
-      action: "opensearch",
-      search: person,
-      limit: "1",
-      namespace: "0",
-      format: "json",
-    };
-
-    // Request to Wikipedia API using axios
-    const wikiSearchResponse = await axios.get(url, { params });
-    const result = wikiSearchResponse.data;
-    if (!result || result[3].length === 0) {
-      return response.redirect("404");
-    }
-
-    const pageUrl = result[3][0];
-    const pageTitle = pageUrl.split("/").pop(); // Extract title from URL
-
-    // Fetch more detailed data for the specific Wikipedia page
-    const pageInfoUrl = `https://en.wikipedia.org/api/rest_v1/page/summary/${pageTitle}`;
-    const pageInfoResponse = await axios.get(pageInfoUrl);
-
-    const pageInfo = pageInfoResponse.data;
-
-    // Sending Wikipedia page summary back as response
-    response.send({
-      title: pageInfo.title,
-      description: pageInfo.description,
-      extract: pageInfo.extract,
-    });
-  } catch (error) {
-    console.error(error);
-    response.redirect("404");
-  }
+server.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}/`);
 });
-
-// 404 page
-app.get("/404", (req, res) => {
-  res.render("404");
-});
-
-// Port listening
-app.listen(3000, () => {
-  console.log("Listening at port 3000...");
-});
-export default app;
